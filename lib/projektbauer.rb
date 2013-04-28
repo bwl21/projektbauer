2a8dcf5398f5b6b965c8c396ce476a231df641c6
Bernhard Weichel <github.com@nospam.weichel21.de>
2013-04-28 22:37:43 +0200

    Added documentation

--- lib/projektbauer.rb
+++ lib/projektbauer.rb
@@ -17,31 +17,34 @@
 # projektbauer add user
 # ...
 #
-# 
+#
 require 'fileutils'
 require 'erb'
 
 class Project
-  attr_accessor  :output_folder, 
-    :server_name,
-    :server_root,
-    :server_admin,
-    :document_root,
-    :project_name,
-    :project_admin_user,
-    :virtual_host,
-    :project_realm,
-    :virtual_host_ip,
-    :virtual_host_domain,
-    :server_port_nossl,
-    :server_port_ssl
+  attr_accessor  :output_folder,    # output folder - currently not in use
+    :server_name,                   # server name of the virtual host <apache>
+    :server_root,                   # root folder on the server <apache>
+    :server_admin,                  # name of the server administrator <apache>
+    :document_root,                 # document root within the location <apache>
+    :project_name,                  # name of the project (please no spaces) \
+    # used for filename
+    :project_admin_user,            # username of the project admin
+    :virtual_host,                  # domain of virtual host
+    :project_realm,                 # realm for the project authentification
+    :virtual_host_ip,               # ip of the virtual host without port
+    :server_port_nossl,             # port for the no ssl access of the virtual host (e.g. 8080)
+    :server_port_ssl                # port of ssel access to the virtual host (e.g. 443)
 
 
 
 
+  #
+  # The class constructor
+  #
+  # @return [Project] returns the created class
   def initialize
 
-    @virtual_host_domain = nil
     @server_port_nossl = nil
     @server_port_ssl = nil
     @server_root = "xxx"
@@ -55,7 +58,18 @@ class Project
 
   end
 
+
+  #
+  # Inititalizes filenames etc. Needs to be called
+  # whenever one or more of the attributes are changed.
+  #
+  # @return [nil] no return
   def init_filenames
+
+    #todo add checks for the attributes
+    # no spaces in project_name
+    #
+
     @_virtual_host_home_dir               = "#{@server_root}/#{@virtual_host}"
     @_include_virtual_hosts_httpd_conf    = "#{@server_root}/include_virtual_hosts.httpd.conf"
     @_project_home                        = "#{@_virtual_host_home_dir}/#{@project_name}"
@@ -68,50 +82,84 @@ class Project
     @_project_svn_authz_file              = "#{@_project_home}/#{@project_name}.dav_svn_authz"
     @_document_root_dir                   = "#{@_virtual_host_home_dir}/www"
     @_index_html_file                     = "#{@_document_root_dir}/index.html"
+
+    nil
   end
 
+
+
+  #
+  # Creates the folders in the file system.
+  # If a folder already exists, nothing happens.
+  #
+  # @return [nil] no return
+  #
   def create_folders
     #todo: double check if path exists
     FileUtils.mkdir_p @_project_svn_dir
     FileUtils.mkdir_p @_project_trac_dir
     FileUtils.mkdir_p @_document_root_dir
-    #todo: crate index.html
     #todo: ensure that folders are not listable
     #todo: htaccess - file
+    nil
   end
 
 
+
+  #
+  # This basically creates the configuration of the virtual host
+  #
+  # @return [Nil] no return
+  #
   def save_virtual_host_httpd_conf
     config = expand_erb("virtual_host_httpd_conf.erb")
 
     File.open(@_virtual_host_httpd_conf,"w"){|f|
       f.puts config
     }
+
+    nil
   end
 
-  # create the entry for the apache configuration
-  def save_project_location_httpd_conf
 
+  #
+  # create the apache configuration for the <location>
+  # It corresponds to a project.
+  #
+  # @return [Nil] no return
+  def save_project_location_httpd_conf
     config = expand_erb("project_location_httpd_conf.erb")
 
     File.open(@_project_location_httpd_conf,"w"){|f|
       f.puts config
     }
 
+    nil
   end
 
-  def create_svn_authz_file
 
-    #todo remove indentation
+  #
+  # creates the Authorization file for svn.
+  # Thereby it creates an observer and a contributor group.
+  # the {Project.project_admin_user} is entered as contributor
+  #
+  # @return [Nil] no return
+  def create_svn_authz_file
     config =expand_erb("svn_authz_file.erb")
 
     File.open(@_project_svn_authz_file, "w") {|f|
       f.puts config
     }
-
+    nil
   end
 
 
+  #
+  # Creates a minimalistic index.html in the virtual host.
+  # In particular this avoids that the files can be listed.
+  #
+  # @return [Nil] no return
+  #
   def create_index_html
     config = expand_erb("index_html.erb")
     File.open(@_index_html_file, "w") {|f|
@@ -120,16 +168,30 @@ class Project
   end
 
 
+  #
+  # Updates the include file with the locations.
+  # This file is subsequently included in the virtual hosts.
+  #
+  # Duplicate entries are removed.
+  #
+  # @return [Nil] no return
   def update_virtual_host_locations_httpd_conf
     maintain_include_file(@_virtual_host_locations_httpd_conf,
                           @_project_location_httpd_conf
                           )
   end
 
+
+  #
+  # Updates the include file with the virtual hosts.
+  # This file needs to be added manually to `/etc/apache2/apache2.conf`
+  #
+  # @return [Nil] no return
   def update_include_virtual_hosts_httpd_conf
     maintain_include_file(@_include_virtual_hosts_httpd_conf,
                           @_project_location_httpd_conf
                           )
+    nil
   end
 
   def create_trac
@@ -139,6 +201,15 @@ class Project
     eos
   end
 
+
+  #
+  # This creates the svn repository if it does not yet exist.
+  # Note that this is tested by investigating the format
+  # folder in the svn repository.
+  #
+  # todo: improve the detection
+  #
+  # @return [type] [description]
   def create_svn
     unless File.exists?(@_project_svn_dir + "/format")
       cmd = "svnadmin create \"#{@_project_svn_dir}\""
@@ -152,6 +223,11 @@ class Project
 
   end
 
+
+  #
+  # This creates the htdigest file
+  #
+  # @return [Nil] no return
   def create_htpasswd
     unless File.exists?(@_project_auth_user_file) then
       cmd = "htdigest -c \"#{@_project_auth_user_file}\"  \"#{@project_realm}\" #{@project_admin_user}"
@@ -162,6 +238,13 @@ class Project
     end
   end
 
+
+
+  #
+  # calling this method after setting all attributes
+  # does the entire generation job
+  #
+  # @return [Nil] no return
   def create_all
     init_filenames
     create_folders
@@ -181,11 +264,31 @@ class Project
 
   private
 
+
+  #
+  # This expands the embedded ruby templated
+  # @param  file [String] Path to the erb template
+  #
+  # @return [String] Expanded Templated
   def expand_erb(file)
     template=ERB.new File.new(File.dirname(__FILE__)+ "/projektbauer/erb/" + file).readlines.join
     template.result(binding)
   end
 
+
+  #
+  # This maintains an include file as follows:
+  #
+  # - the file is read
+  # - the new entry is appended
+  # - the result is sorted an duplicates are removed
+  # - the file is saved if it is different than before
+  #
+  # @param  include_file [String] Name if the include file to be maintained
+  # @param  entry [String] The new entry which shall be included
+  #
+  # @return [Nil] no return
+  #
   def maintain_include_file(include_file, entry)
     if File.exists?(include_file) then
       old = File.open(include_file).readlines.map{|i| i.strip}
@@ -203,6 +306,7 @@ class Project
         f.puts new.join("\n")
       }
     end
+    nil
   end
 
   def _create_post_commit(file)
